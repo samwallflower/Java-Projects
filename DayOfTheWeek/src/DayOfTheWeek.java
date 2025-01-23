6af7d28dcbacae2bc8f0dff6be086c8dd5824336
Samanta <samwallflower197127@gmail.com>
2025-01-23 16:16:43 +0100

    Second Commit

--- DayOfTheWeek/src/DayOfTheWeek.java
+++ DayOfTheWeek/src/DayOfTheWeek.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class DayOfTheWeek {
     public static void main(String[] args) {
+        System.out.println("Hello");
 
        Scanner sc = new Scanner(System.in);
        System.out.println("Give a number: ");
